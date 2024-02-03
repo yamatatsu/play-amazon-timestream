69fd6c37cab8834fadb55b964d5e0f946e1551a5
yamatatsu <the.kinnikuman@gmail.com>
2024-02-03 20:48:53 +0900

    setup biome

--- cdk.ts
+++ cdk.ts
@@ -1,20 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import * as timestream from 'aws-cdk-lib/aws-timestream';
+import * as cdk from "aws-cdk-lib";
+import * as timestream from "aws-cdk-lib/aws-timestream";
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'play-amazon-timestream');
+const stack = new cdk.Stack(app, "play-amazon-timestream");
 
-const database = new timestream.CfnDatabase(stack, 'Database', {
-  databaseName: 'play-amazon-timestream'
+const database = new timestream.CfnDatabase(stack, "Database", {
+	databaseName: "play-amazon-timestream",
 });
-new timestream.CfnTable(stack, 'Table', {
-  databaseName: database.databaseName!,
-  tableName: 'play-amazon-timestream',
-  retentionProperties: {
-    memoryStoreRetentionPeriodInHours: 1,
-    magneticStoreRetentionPeriodInDays: 1
-  },
-  magneticStoreWriteProperties: {
-    enableMagneticStoreWrites:false
-  }
+new timestream.CfnTable(stack, "Table", {
+	databaseName: database.ref,
+	tableName: "play-amazon-timestream",
+	retentionProperties: {
+		memoryStoreRetentionPeriodInHours: 1,
+		magneticStoreRetentionPeriodInDays: 1,
+	},
+	magneticStoreWriteProperties: {
+		enableMagneticStoreWrites: false,
+	},
 });
